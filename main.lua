e92b8a3fc489a0d3f51da2c8e5b3dfcceabfd1a4
jordan4ibanez <jordan4ibanez@gmail.com>
2018-06-04 21:19:31 -0400

    Add sprites

--- main.lua
+++ main.lua
@@ -12,7 +12,7 @@ survival.Water  = math.random(50,100)
 resource_tick   = 0
 
 map = {}
-map.blocksize = 200
+map.blocksize = 200 --length and width
 map.loadedblock = {}
 
 function time_tick(dt)
@@ -21,12 +21,21 @@ end
 
 function love.load()
 	love.graphics.setNewFont(20)
+	
+ 
+    --if success then
+        -- If the game is fused and it's located in C:\Program Files\mycoolgame\,
+        -- then we can now load files from that path.
+    cobble = love.graphics.newImage("cobble_blood_1_new.png")
+    brick = love.graphics.newImage("brick_brown_0.png")
+ --   end
+	
 end
 
 function love.draw(dt)
 	--love.graphics.print(tostring(math.maxinteger), 40,50)
-	love.graphics.print(tostring(love.math.noise( 100/map.blocksize,100/map.blocksize)),10,60)
-
+	--love.graphics.print(tostring(love.math.noise( 100/map.blocksize,100/map.blocksize)),10,60)
+	love.generateblock(dt,1,1)
 end
 
 function love.update(dt)
@@ -36,8 +45,22 @@ end
 --generates map block in memory
 function love.generateblock(dt,x,y)
 	map.loadedblock = {}
-	for xer = 1,map.blocksize do
-		love.graphics.print(tostring(xer),10,50)
+	
+	
+	for yer = 1,map.blocksize do
+		map.loadedblock[tostring(yer)] = {}
+		for xer = 1,map.blocksize do
+			--map.loadedblock[yer][xer] =
+			local value = love.math.noise(xer,yer )
+			if value > 0.4 then
+				--love.graphics.setColor( 255, 255, 255 )
+				love.graphics.draw(cobble, xer*32, yer*32)
+				--love.graphics.print("O",(xer*17),(yer*17))
+			else
+				--love.graphics.setColor( 255, 0, 0 )
+				love.graphics.draw(brick, xer*32, yer*32)
+			end
+		end
 	end
 end
 
